da620ebb9ff86939e707112050ad5b0d13834e8d
Daniel <daniel@daniel-1215T.(none)>
2012-01-26 20:52:25 -0300

    Directory fix

--- records/admin.py
+++
@@ -1,4 +0,0 @@
-from models import Record
-from django.contrib import admin
-
-admin.site.register(Record)
--- records/import_csv.py
+++
@@ -1,9 +0,0 @@
-import csv
-reader = csv.reader(open('programming-sample.tab', 'rb'))
-for index,row in enumerate(reader):
-    print 'Record: ' + str(index + 1)
-    print '------------'
-    print 'Title: ' + row[0]
-    print 'Month: ' + row[1]
-    print 'Day: ' + row[2]
-    print 'Body: ' + row[3]
