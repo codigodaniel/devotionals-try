50a494e3e8c5e7950d4cf3292df5be2a644f1690
Daniel <daniel@daniel-1215T.(none)>
2012-01-26 20:04:12 -0300

    taggit added

--- settings.py
+++ settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'records',
+    'taggit',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
